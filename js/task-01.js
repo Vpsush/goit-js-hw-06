9dcf839382afe736cbf3605a8faba74865f41a7c
Vpsush <vpsush2023@gmail.com>
2023-07-17 23:00:44 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,10 @@
-const countCategory = document.querySelector("li.item"){
-    return `Number of categories: ${countCategory.length}`
-};
-console.log(countCategory);
+const countCategory = document.querySelectorAll(".item");
+console.log(`Number of categories: ${countCategory.length}`);
+
+const categories = [...countCategory]
+  .map(
+    (categories) => `Category: ${categories.children[0].textContent}
+    Elements: ${categories.children[1].children.length}`
+  )
+  .join("\n");
+console.log(categories);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const creatingLi = document.createElement("li"),
+creatingLi.textContent = split.ingredients;
+
+console.log(creatingLi);

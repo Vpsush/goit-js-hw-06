bd08896b51341082bd76aaafd41e806ae93579ba
Vpsush <vpsush2023@gmail.com>
2023-07-19 12:00:29 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,7 +6,14 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const creatingLi = document.createElement("li"),
-creatingLi.textContent = split.ingredients;
 
-console.log(creatingLi);
+const ingredientsList = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  return liEl;
+});
+
+console.log(elements);
+ingredients.append(...elements);

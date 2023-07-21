2ab0fd8df1aa698e4108e233199763c4970b4a0b
Vpsush <vpsush2023@gmail.com>
2023-07-21 23:55:46 +0300

    task 8 dane

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+// const form = document.querySelector(".login-form");
+// form.addEventListener("submit", writeIntoForm);
 
+// function writeIntoForm(event) {
+//   if ((form.hasAttribute(email) && form.hasAttribute(password)) === "") {
+//     alert("All fields must be fill");
+
+//   }
+// }
+
+// const form = document.querySelector(".login-form");
+// Define the validateForm function to handle form validation
+function validateForm(event) {
+  // Prevent the form from being submitted to the server
+  event.preventDefault();
+
+  // Get the values of the email and password fields
+  const emailInput = document.getElementsByName("email")[0];
+  console.log(document.getElementsByName("email"));
+  const emailValue = emailInput.value;
+  const passwordInput = document.getElementsByName("password")[0];
+  const passwordValue = passwordInput.value;
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("Please enter both email and password.");
+  } else {
+    const obectForm = { emailValue, passwordValue };
+    console.log(obectForm);
+  }
+}
+const form = document.querySelector(".login-form");
+form.onsubmit = validateForm;

bbaddb1156845393bf73db88b8bd091b13d18197
Vpsush <vpsush2023@gmail.com>
2023-07-21 20:55:13 +0300

    task5 done

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  nameUser: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener("input", (event) => {
+  refs.nameUser.textContent = event.currentTarget.value;
+
+  if (event.currentTarget.value === " ") return "Anonymous";
+});
